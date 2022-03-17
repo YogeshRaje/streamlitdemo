d4be09bdf4123b70f56de45be6d22f7d48fb43db
Yogesh Raje <yhraje@gmail.com>
2022-03-17 09:47:25 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import joblib
+
+st.title('Netwrok Ad purchased or Not ')
+c=['Purchased'],['Not Purchased']
+
+#age=st.number_input("Enter Age of customer  ",0.0)
+#salary=st.number_input("Enter Estimated Salary",0.0)
+
+age=st.slider("Enter Age of customer  ",10,100)
+salary=st.slider("Enter Estimated Salary",1500,15000)
+
+loaded_model=joblib.load(open('Final_model.pkl','rb'))
+
+prediction= loaded_model.predict([[age,salary]])
+
+st.write('Predicted class is : ',c[prediction[0]])
